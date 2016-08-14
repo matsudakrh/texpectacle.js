1c4f95c58bfa834426b246ea77653d42066c6c10
matsuda <matsuda.ddcm@gmail.com>
2016-08-14 17:00:58 +0900

    読み込むファイルをsample.min.cssに変更、スタイルの修正

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -7,6 +7,6 @@ const notify  = require('gulp-notify');
 gulp.task( 'jade', function () {
     return gulp.src( ['source/jade/**/*.jade', '!source/jade/**/_*.jade'], { base: 'source/jade/'})
         .pipe( plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(jade())
+        .pipe( jade())
         .pipe( gulp.dest('public/'));
 });
