c0bae83135178c30c5b504746d73d0034f3bc5ad
matsuda <matsuda.ddcm@gmail.com>
2016-08-17 16:29:17 +0900

    no message

--- assets/js/texpectacle.js
+++ assets/js/texpectacle.js
@@ -109,7 +109,6 @@ var Texpectacle_Class = function () {
             if (this.endTimer) {
                 clearTimeout(this.endTimer);
             }
-
             this.insertText();
             this.index = 0;
             this.getClassName();
@@ -165,6 +164,8 @@ var Texpectacle_Class = function () {
         key: 'setScroll',
         value: function setScroll() {
             var _this3 = this;
+
+            this.insertText();
 
             var resizeTimer = false;
             var scrollTimer = false;
